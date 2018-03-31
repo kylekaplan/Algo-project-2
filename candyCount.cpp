c42e2bd125c3cfc1e92eab990c4d6ce8aa27900d
kylekaplan <kyle@collector.org>
2018-03-31 01:37:50 -0400

    mostly working

--- candyCount.cpp
+++ candyCount.cpp
@@ -8,6 +8,11 @@
 
 #include <iostream>
 #include <fstream>
+#include <string>
+#include <cstring>
+#include <sstream>
+#include <vector>
+#include<cmath>
 using namespace std;
 /*
  * Algorithm to count the number of unique arrangements of
@@ -17,70 +22,73 @@ using namespace std;
  * @l, the number of long candies
  * @return the number of possible samplers
  */
-long long naiiveAlgo(int n, int s, int l) {
-    // Initialize the Arr.
-    long long int Arr[n+1];
+
+int naiiveAlgo(int n, int s, int l) {
+    int answer;
+    int index;
+    n = n + 1;
+    index = n;
+    int* a = NULL;   // Pointer to int, initialize to nothing.
+    a = new int[index];  // Allocate n ints and save ptr in a.
 
     // Set the first position of the array to 1.
-    Arr[0] = 1;
+    a[0] = 1;
+    cout << "first is " << a[0];
     // Set the second position of the array to the number of squares.
-    Arr[1] = s;
+    a[1] = s;
     // For loop that iterates from the second position of the array till
     // it is less than or equal to the size of the sampler.
-    for(int i=2; i <=n; i++){
+    for(int i=2; i <=n-1; i++){
         //Set the right ride of the equation to the position at i.
-        Arr[i]=s*Arr[i-1]+l*Arr[i-2];
+        a[i]=(s*a[i-1])+(l*a[i-2]);
+        cout << "iter num " << i << " is " << a[i] << endl;
     }
+    answer = a[n - 1];
+    delete [] a;  // When done, free memory pointed to by a.
+    *a = NULL;     // Clear a to prevent using invalid memory reference.
     //return the number of possible samplers.
-    return Arr[n];
+//    cout << "answer: " << answer << endl;
+    return answer;
 }
 
 /**
  * Main Function
  */
 int main() {
-
-    // Initialize variables.
-    ifstream input;
     ofstream output;
-    int p, size, squareCandy, longCandy;
-    char inputFilename[] = "input.txt";
-    char outputFilename[] = "output.txt";
-
-    // Open the file for reading.
-    input.open(inputFilename);
-    // Base Case if file does not exist.
-    if (!input.is_open()){
-        cout << "Can't open input file input.txt!\n";
-        exit(1);
-    }
-
     // Open the file for writing.
-    output.open(outputFilename);
+    output.open("../output.txt");
     // Base Case if file can not be created.
     if (!output.is_open()) {
         cout << "Can't open output file!\n";
         exit(1);
     }
 
-    // Gets the next character (an unsigned char) from the specified stream
-    input >> p;
-    // if loop to read in the files while it does not reach end of file
-    // and is not equal the the new line character
-    for(int i =0; i<p; i++){
-        // Reads formatted input from a stream and sets the value from the input
-        // file to size, squareCandy and longCandy
-        input >> size;
-        input >> squareCandy;
-        input >> longCandy;
-        // Sets the value (number of possible samplers) returned from
-        // naiiveAlgo to results.
-        long long int results = naiiveAlgo(size, squareCandy, longCandy);
-        // Adds the value of result into the output file.
-        output << results << endl;
+    std::ifstream infile("../input.txt");
+
+    bool first = true;
+    int p;
+    std::string line;
+    while (std::getline(infile, line))
+    {
+        if(first) {
+            first = false;
+            p = stoi(line);
+        } else {
+            std::istringstream iss(line);
+            int size;
+            int squareCandy;
+            int longCandy;
+//            if (!(iss >> first_on_line >> second_on_line >> third_on_line)) { break; } // error
+            if (iss >> size >> squareCandy >> longCandy) {
+                cout << size;
+                int results = naiiveAlgo(size, squareCandy, longCandy);
+                // Adds the value of result into the output file.
+//                cout << results;
+                output << results << endl;
+            }
+        }
     }
-    // Closes the input file.
-    input.close();
     // Closes the output file.
     output.close();
     return 0;
